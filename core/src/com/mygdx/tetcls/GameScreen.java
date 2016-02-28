efdfd719cab9ce1844ae7ce3d186fea9e09b5432
Ent <last_ent@outlook.com>
2016-02-28 22:50:25 +0530

    Halfway commit where Block I works & colors on hitting bottom. But passive grid is not yet registered.

---
+++ core/src/com/mygdx/tetcls/GameScreen.java
@@ -0,0 +1,151 @@
+package com.mygdx.tetcls;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import com.mygdx.tetcls.blocks.BlockLine;
+
+public class GameScreen extends ScreenAdapter {
+
+    private static float timer = GameConstants.SECOND;
+    private boolean isSpacePressed = false;
+    private boolean isLeftPressed = false;
+    private boolean isRightPressed = false;
+    private boolean isDownPressed = false;
+    private Rectangle tetrisArena = new Rectangle(
+            GameConstants.GAME_SCREEN_START_X, GameConstants.GAME_SCREEN_START_Y,
+            GameConstants.GAME_WIDTH, GameConstants.GAME_HEIGHT
+    );
+    private Grid arenaGrid;
+
+    private ShapeRenderer shapeRenderer;
+    private Viewport viewport;
+    private Camera camera;
+    private SpriteBatch batch;
+
+    private BlockLine blockLine;
+
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override
+    public void show() {
+        camera = new OrthographicCamera();
+        camera.position.set(GameConstants.WORLD_WIDTH / 2, GameConstants.WORLD_HEIGHT / 2, 0);
+        camera.update();
+
+        viewport = new FitViewport(GameConstants.WORLD_WIDTH, GameConstants.WORLD_HEIGHT, camera);
+        shapeRenderer = new ShapeRenderer();
+        batch = new SpriteBatch();
+
+
+//        createNewBlock();
+        blockLine = new BlockLine(GameConstants.BLOCK_START_X, GameConstants.BLOCK_START_Y/3);
+        arenaGrid = new Grid(tetrisArena);
+    }
+
+    @Override
+    public void render(float delta) {
+        clearScreen();
+        batch.setProjectionMatrix(camera.projection);
+        batch.setTransformMatrix(camera.view);
+        batch.begin();
+        batch.end();
+
+        shapeRenderer.setProjectionMatrix(camera.projection);
+        shapeRenderer.setTransformMatrix(camera.view);
+        shapeRenderer.setAutoShapeType(true);
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+
+        updateOnOneSecond(delta);
+        updateOnDelta(delta);
+        shapeRenderer.end();
+    }
+
+    private void clearScreen() {
+        Gdx.gl.glClearColor(Color.BLACK.r, Color.BLACK.g, Color.BLACK.b, Color.BLACK.a);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+    public void updateOnOneSecond(float delta) {
+        timer -= delta;
+        if (timer > 0) return;
+
+        timer = GameConstants.SECOND;
+
+        blockLine.updateOnSecond();
+    }
+    public void createNewBlock(){
+        blockLine = new BlockLine(GameConstants.BLOCK_START_X, GameConstants.BLOCK_START_Y);
+    }
+
+    public void drawPassiveBlocks(){
+        Rectangle[][] grid = arenaGrid.getGrid();
+        Rectangle rect;
+        shapeRenderer.set(ShapeRenderer.ShapeType.Filled);
+        shapeRenderer.setColor(Color.LIGHT_GRAY.r, Color.LIGHT_GRAY.g, Color.LIGHT_GRAY.b, Color.LIGHT_GRAY.a);
+        for(int x=0; x< GameConstants.GRID_COLUMNS; x++)
+            for(int y = 0; y < GameConstants.GRID_ROWS; y++){
+                rect = grid[x][y];
+                if(rect != null) {
+                    shapeRenderer.rect(rect.getX(), rect.getY(), GameConstants.BLOCK_SIDE_SIZE, GameConstants.BLOCK_SIDE_SIZE);
+                }
+            }
+        shapeRenderer.set(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.WHITE.r, Color.WHITE.g, Color.WHITE.b, Color.WHITE.a);
+    }
+    public void renderGrid() {
+        shapeRenderer.setColor(Color.YELLOW.r, Color.YELLOW.g, Color.YELLOW.b, 0.5f);
+        // Create Rows :: x1 & x2 <= CONST & y1 == y2 <= [0,  GAME_HEIGHT, block_side]
+        for(int row= 0; row < GameConstants.GAME_HEIGHT; row += GameConstants.BLOCK_SIDE_SIZE)
+            shapeRenderer.line(GameConstants.GAME_SCREEN_START_X,
+                    GameConstants.BLOCK_START_Y - row,
+                    GameConstants.GAME_SCREEN_START_X + GameConstants.GAME_WIDTH,
+                    GameConstants.BLOCK_START_Y - row);
+
+        // Create Columns :: y1 & y2 <= CONST & x1 == x2 <= [0, Game_Width, block_side]
+        for(int column = 0; column < GameConstants.GAME_WIDTH; column += GameConstants.BLOCK_SIDE_SIZE)
+            shapeRenderer.line(
+                    GameConstants.GAME_SCREEN_START_X + column,
+                    GameConstants.BLOCK_START_Y,
+                    GameConstants.GAME_SCREEN_START_X + column,
+                    GameConstants.BLOCK_START_Y - GameConstants.GAME_HEIGHT
+            );
+        shapeRenderer.setColor(Color.WHITE);
+    }
+
+    public void updateOnDelta(float delta) {
+        renderGrid();
+        checkForEvents(delta);
+        blockLine.updateOnDelta(delta, isSpacePressed, isLeftPressed, isRightPressed, isDownPressed, tetrisArena);
+        shapeRenderer.rect(tetrisArena.getX(), tetrisArena.getY(), tetrisArena.width, tetrisArena.height);
+
+        if(blockLine.isBlockResting()) {
+            arenaGrid.register(blockLine);
+            createNewBlock();
+        }
+        blockLine.render(shapeRenderer);
+        drawPassiveBlocks();
+    }
+
+    public void checkForEvents(float delta){
+        isSpacePressed = Gdx.input.isKeyJustPressed(Input.Keys.SPACE);
+        isLeftPressed = Gdx.input.isKeyJustPressed(Input.Keys.LEFT);
+        isRightPressed = Gdx.input.isKeyJustPressed(Input.Keys.RIGHT);
+        isDownPressed = Gdx.input.isKeyJustPressed(Input.Keys.DOWN);
+//        if(isDownPressed) System.out.println(isDownPressed);
+    }
+}
---
+++ core/src/com/mygdx/tetcls/blocks/BlockLine.java
@@ -0,0 +1,187 @@
+package com.mygdx.tetcls.blocks;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.tetcls.GameConstants;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Created by HP-430 on 2/25/2016.
+ */
+
+/***
+ * DONE: Next is to write a comparator/sorter so that the blocks are returned in order of x & y.
+ */
+public class BlockLine {
+    private Rectangle[] rects = new Rectangle[4];
+    private boolean isResting = false;
+    private boolean isVertical = true;
+    private boolean isFast = false;
+    private static Comparator<Rectangle> BlockLineComparator = new Comparator<Rectangle>() {
+        @Override
+        public int compare(Rectangle o1, Rectangle o2) {
+            float compareValue = o1.getY() - o2.getY();
+
+            if (compareValue == 0)
+                compareValue = o1.getX() - o2.getX();
+
+            if (compareValue > 0) compareValue = +1;
+            else if (compareValue < 0) compareValue = -1;
+            else compareValue = 0;
+
+            return (int) compareValue;
+        }
+    };
+
+    public BlockLine(float x, float y) {
+        for(int i = 0; i < GameConstants.BLOCK_NUM; i++) {
+            rects[i] =new Rectangle(0, 0, GameConstants.BLOCK_SIDE_SIZE, GameConstants.BLOCK_SIDE_SIZE);
+        }
+        setX(x);
+        setY(y - GameConstants.BLOCK_SIDE_SIZE*3, GameConstants.BLOCK_SIDE_SIZE);
+    }
+    public void setX(float x) {
+        if(isVertical)
+            for(Rectangle rect : getBlocks()) rect.setX(x);
+        else {
+            int step = 0;
+            for(Rectangle rect : getBlocks()) {
+                rect.setX(x + step);
+                step += GameConstants.BLOCK_SIDE_SIZE;
+            }
+
+        }
+    }
+
+    /***
+     *
+     * @param y - Rendering of the blocks start from BLOCK_SIDE_SIZE pixels below the given value of y.
+     * @param step
+     */
+    public void setY(float y, float step){
+        if(isVertical) {
+            for (Rectangle rect : rects) {
+                rect.setY(y - step);
+                step -= GameConstants.BLOCK_SIDE_SIZE;
+            }
+        } else {
+            for(Rectangle rect : rects)
+                rect.setY(y - step);
+        }
+    }
+
+    public float getX(){
+        return getLowestBlock().getX();
+    }
+    public void correctYAfterFast(){
+        float y = getLowestBlock().getY();
+//        System.out.println(y);
+        float position = y / GameConstants.BLOCK_SIDE_SIZE;
+        int next = (int) position;
+        int  min= next +1;
+        float limbo = position - next;
+        float newY = (limbo >= .5f? next : min) * GameConstants.BLOCK_SIDE_SIZE;
+//        System.out.println("min: "+ min + " next: " + next + " newY: " + newY+ " position: "+ position + " limbo: " + limbo);
+//        System.out.println(y - newY  );
+//        System.out.println("\tmin: "+ min * GameConstants.BLOCK_SIDE_SIZE+ " next: " + next * GameConstants.BLOCK_SIDE_SIZE+ " newY: " + newY* GameConstants.BLOCK_SIDE_SIZE);
+        setY(newY, GameConstants.BLOCK_SIDE_SIZE);
+    }
+
+    public void updateOnSecond() {
+        if(isFast){
+            isFast = false;
+            correctYAfterFast();
+        } else {
+            setY(getLowestBlock().getY(), GameConstants.BLOCK_SIDE_SIZE);
+        }
+        setX(getLeftmostBlock().getX());
+    }
+
+    public void updateOnDelta(float delta, boolean toTurn, boolean toLeft,
+                              boolean toRight, boolean isDownPressed, Rectangle arena) {
+        if (toTurn) isVertical = !isVertical;
+        if (toLeft) setX(getLeftmostBlock().getX() - GameConstants.BLOCK_SIDE_SIZE);
+        else if (toRight) setX(getLeftmostBlock().getX() + GameConstants.BLOCK_SIDE_SIZE);
+        if(isDownPressed) isFast = true;
+        if (isFast)
+            setY(
+                getLowestBlock().getY(),
+                GameConstants.BLOCK_SIDE_SIZE  * delta * GameConstants.BLOCK_SPEED_SCROLLING_FACTOR
+            );
+        correctCollision(arena);
+    }
+
+    public void render(ShapeRenderer shapeRenderer) {
+        for(Rectangle rect : rects){
+            shapeRenderer.rect(rect.getX(), rect.getY(), rect.width, rect.height);
+        }
+    }
+
+    /***
+     * Catch all method to deal with
+     * * Stop once the block hits the bottom.
+     * * Nudge the block to left or right depending on which wall it hits.
+     * * ***More to Come***
+     */
+    public void correctCollision(Rectangle arena) {
+        Rectangle lowestBlock = getLowestBlock();
+        Rectangle rightmostBlock = getRightmostBlock();
+        Rectangle leftmostBlock = getLeftmostBlock();
+
+        if (lowestBlock.getY() < arena.getY()) {
+            setY(arena.getY() + GameConstants.BLOCK_SIDE_SIZE, GameConstants.BLOCK_SIDE_SIZE);
+            isResting = true;
+        }
+        if (leftmostBlock.getX() <= arena.getX()) {
+            setY(lowestBlock.getY()  + GameConstants.BLOCK_SIDE_SIZE, GameConstants.BLOCK_SIDE_SIZE);
+            setX(arena.getX());
+        }
+        float rightBlockX = rightmostBlock.getX() + rightmostBlock.getWidth();
+//        System.out.println("rb.x: " + rightmostBlock.getX() +" ar.x: " + (arena.getX() + arena.getX() ) + " arena.getWidth() + arena.getX()? " + ( rightBlockX  >= arena.getWidth() + arena.getX()));
+        if( rightBlockX  > arena.getWidth() + arena.getX()){
+            float arenaXEnd = arena.getX() + arena.getWidth();
+            float blocksToNudge = GameConstants.BLOCK_SIDE_SIZE *(isVertical? 1: 4);
+
+            setX(arenaXEnd - blocksToNudge);
+        }
+    }
+
+    public Rectangle getLowestBlock(){
+        Rectangle lowestRect = rects[0];
+        for(Rectangle rect : rects)
+            if(rect.getY() < lowestRect.getY())
+                lowestRect = rect;
+        return lowestRect;
+    }
+
+    public Rectangle getLeftmostBlock() {
+        Rectangle leftmostRect = rects[0];
+
+        for(Rectangle rect: rects)
+            if (rect.getX() < leftmostRect.getX() ||
+                    (rect.getX() == leftmostRect.getX() && rect.getY() < leftmostRect.getY()))
+                leftmostRect = rect;
+        return leftmostRect;
+    }
+
+    public Rectangle getRightmostBlock() {
+        Rectangle rightmostRect = rects[0];
+        for(Rectangle rect : rects)
+            if(rect.getX() > rightmostRect.getX() ||
+                    rect.getX() == rightmostRect.getX() && rect.getY() < rightmostRect.getY())
+                rightmostRect = rect;
+        return rightmostRect;
+    }
+
+    public Rectangle[] getBlocks() {
+        Arrays.sort(rects, BlockLineComparator);
+        return rects;
+    }
+    public boolean isBlockResting(){
+        return isResting;
+    }
+
+}
